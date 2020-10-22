53de20c4a6fefcbf1afd738b0ef01862f5705941
reitmas32 <rafa.zamora.rals@gmail.com>
2020-10-21 22:09:47 -0500

    Se comenzara a hacer la nueva version con RUST y el patron Command

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-use std::env;
-use std::fs;
+
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    let contents = fs::read_to_string("assets/help.next")
-    .expect("Something went wrong reading the file");
-
-    println!("{}", contents);
+    println!("HOLA MUNDO");
 }
+
