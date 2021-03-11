1bded65139924ccb8d20c1945479ef213da92eaf
reitmas32 <rafa.zamora.rals@gmail.com>
2021-03-11 10:11:46 -0600

    feat(Manager): Se actualizaron algunos comandos

--- src/manager_command.cpp
+++ src/manager_command.cpp
@@ -7,15 +7,13 @@ ManagerCommand::~ManagerCommand()
     this->stackCommand.clear();
 }
 
-void
-ManagerCommand::execute()
+void ManagerCommand::execute()
 {
 
     this->stackCommand.execute();
 }
 
-void
-ManagerCommand::initialize(int argc, char const* argv[])
+void ManagerCommand::initialize(int argc, char const *argv[])
 {
 
     std::string list_args;
@@ -24,30 +22,70 @@ ManagerCommand::initialize(int argc, char const* argv[])
     if (argc < 2)
         return;
 
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++)
+    {
         std::string line = argv[i];
-        if (this->isFlag(argv[i])) {
-            this->createFlag(argv[i]);
-            continue;
-        } else if (line == "create") {
+        if (line == "create")
+        {
             this->stackCommand.append(new PathCommand());
             this->stackCommand.append(new CreateProjectCommand(argv[i + 1]));
             this->stackCommand.append(new ReadHelpCommand());
-        } else if (line == "mcreate") {
+        }
+        else if (line == "mcreate")
+        {
             this->stackCommand.append(new PathCommand());
             this->stackCommand.append(new MCreateProjectCommand(argv[i + 1]));
             this->stackCommand.append(new ReadHelpCommand());
-        } else if (line == "build") {
-            this->stackCommand.append(new BuildCommand());
-        } else if (line == "run") {
+        }
+        else if (line == "build")
+        {
+            if (read_conf() == -1)
+                return;
+            std::string typeBuild;
+            if (argv[i + 1] != nullptr)
+            {
+                typeBuild = argv[i + 1];
+            }
+            this->stackCommand.append(new BuildCommand(typeBuild));
+        }
+        else if (line == "run")
+        {
+            if (read_conf() == -1)
+                return;
             this->stackCommand.append(new RunCommand());
-        } else if (line == "import" || line == "i") {
+        }
+        else if (line == "clear" || line == "-cl")
+        {
+            if (read_conf() == -1)
+                return;
+            this->stackCommand.append(new ClearCommand());
+        }
+        else if (line == "import" || line == "i")
+        {
             this->stackCommand.append(new ImportCommand(argv[i + 1]));
-        } else if (line == "upgrade" || line == "u") {
+        }
+        else if (line == "upgrade" || line == "u")
+        {
             this->stackCommand.append(new PathCommand());
             this->stackCommand.append(new UpgradeCommand());
             this->stackCommand.append(new VersionCommand());
-        } else {
+        }
+        else if (line == "--help" || line == "-h")
+        {
+            this->stackCommand.append(new PathCommand());
+            this->stackCommand.append(new ReadHelpCommand());
+        }
+        else if (line == "--version" || line == "-v")
+        {
+            this->stackCommand.append(new PathCommand());
+            this->stackCommand.append(new VersionCommand());
+        }
+        else if (line == "--path" || line == "-p")
+        {
+            this->stackCommand.append(new PathCommand());
+        }
+        else
+        {
             list_args += " " + line;
         }
     }
