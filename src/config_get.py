963d4d6f356f94cd5f5a6ec501b08e7d59a4ad11
reitmas32 <rafa.zamora.rals@gmail.com>
2022-01-08 14:57:55 -0600

    feat(config_get.py): Create get function for config

---
+++ src/config_get.py
@@ -0,0 +1,35 @@
+######################################################################
+### author = Rafael Zamora 
+### copyright = Copyright 2020-2022, Next Project 
+### date = 08/01/2022
+### license = PSF
+### version = 3.0.0 
+### maintainer = Rafael Zamora 
+### email = rafa.zamora.ram@gmail.com 
+### status = Production
+######################################################################
+
+#System Packages
+import os
+
+#Local Packages
+import read_config
+
+def get(property):
+
+    # default value of property
+    value_of_property = "null"
+
+    # alone Next version
+    if(property != ""):
+
+        # View the dir of current project {name}
+        dir_project = os.getcwd()
+
+        #Read config of current project
+        config_obj = read_config.read_config(dir_project)
+
+        #Wrapper for properties
+        value_of_property = config_obj.option(property)
+
+    return value_of_property
