5ef257e6d9f66880c15dd1b2d6c873bc2e6e459c
reitmas32 <rafa.zamora.rals@gmail.com>
2022-01-13 19:20:37 -0600

    feat(config_get.py): Create get function for config

--- src/build_next.py
+++ src/build_next.py
@@ -24,17 +24,17 @@ def build():
             config_obj = read_config.read_config(this_dir)
 
             if config_obj != False:
-                os.mkdir(config_obj.build_dir)
+                os.mkdir(config_obj.get("build_dir"))
 
-                os.chdir(config_obj.build_dir)
+                os.chdir(config_obj.get("build_dir"))
 
                 subprocess.run([
                     "cmake", this_dir + "/.", 
-                    "-G" + config_obj.build_system, 
-                    "-DCMAKE_CXX_COMPILER=" +  config_obj.cxx_compiler, 
-                    "-DCMAKE_C_COMPILER=" +  config_obj.c_compiler] + config_obj.cmake_flags)
+                    "-G" + config_obj.get("build_system"), 
+                    "-DCMAKE_CXX_COMPILER=" +  config_obj.get("cxx_compiler"), 
+                    "-DCMAKE_C_COMPILER=" +  config_obj.get("c_compiler")] + config_obj.get("cmake_flags"))
 
-                subprocess.run([config_obj.build_system_exe] + config_obj.build_system_flags)
+                subprocess.run([config_obj.get("build_system_exe")] + config_obj.get("build_system_flags"))
 
         except OSError as exc:
             print(exc)
