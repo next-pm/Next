bb61bcf67f039f1935cc45974d014b11404fcd8e
reitmas32 <rafa.zamora.rals@gmail.com>
2022-03-20 14:55:35 +0000

    Doc(config_set.py): Documentation completed and added CLI messages

--- src/config_set.py
+++ src/config_set.py
@@ -1,9 +1,9 @@
 ######################################################################
 ### author = Rafael Zamora 
 ### copyright = Copyright 2020-2022, Next Project 
-### date = 08/01/2022
+### date = 20/03/2022
 ### license = PSF
-### version = 3.0.0 
+### version = 3.2.0 
 ### maintainer = Rafael Zamora 
 ### email = rafa.zamora.ram@gmail.com 
 ### status = Production
@@ -15,8 +15,18 @@ import os
 #Local Packages
 import read_config
 import write_config
+import tools
 
 def set(property, value):
+    """Set a property to the current project
+
+    Args:
+        property (str): name of property
+        value (str): value of property
+
+    Returns:
+        value_of_property([str, null]): new value of property
+    """
 
     # default value of property
     value_of_property = "null"
@@ -33,7 +43,18 @@ def set(property, value):
         #Wrapper for properties
         value_of_property = config_obj.set(property, value)
 
+        #If it was set correctly
         if(value_of_property != "null"):
+            
+            #Write the new config
             write_config.write_property(config_obj, dir_project)
+            
+            # Message(Successful): Set property
+            tools.message_successful('Set property ' + property + ': ' + value)
+        
+        else:
+            # Message(Error): Could not set
+            tools.message_error('Could not set ' + property + ': ' + value)
 
+    #Value of new property ([str, null])
     return value_of_property
