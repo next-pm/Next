0a7a504a0d21a1294354cc102197e7edbc910b1b
reitmas32 <rafa.zamora.rals@gmail.com>
2022-03-28 16:28:35 +0000

    Doc(write_config.py): Documentation completed and added CLI messages

--- src/write_config.py
+++ src/write_config.py
@@ -1,9 +1,9 @@
 ######################################################################
 ### author = Rafael Zamora 
 ### copyright = Copyright 2020-2022, Next Project 
-### date = 08/01/2022
+### date = 28/03/2022
 ### license = PSF
-### version = 3.0.0 
+### version = 3.2.0 
 ### maintainer = Rafael Zamora 
 ### email = rafa.zamora.ram@gmail.com 
 ### status = Production
@@ -16,58 +16,56 @@ import os
 import tools    
 
 def _this_is_a_dir(dir):
+    """Identify if this is Dir
+
+    Args:
+        dir (str): Direction of proyect
+
+    Returns:
+        bool: Flag
+    """
+    
+    # Flag 
     done = False
 
+    # Is a Directory
     if os.path.isdir(dir):
         done = True
-    else:
-        print(dir + " Not is a directory")
-    
-    return done
-
-def _exists_config_file(dir):
-    done = False
-    try:
-        config_file = open( dir + "/config.yaml", "r")
-        config_file.close()
-        done = True
-    except:
-        print(dir + " Not is a NextProject")
-    
-    return done
-
-def _clear_config_file(dir):
-        config_file = open( dir + "/config.yaml", "w")
-        config_file.close()
-
-def write_config(config_obj, dir):
-    # pasos 
-    # 1.- identificar si el directorio es un NextPackage o NextProject
-    # 2.- buscar el archivo config.yaml
-    if _this_is_a_dir(dir):
-
-        config_map = config_obj.to_map()
-                
-        with open('config.yaml', 'w') as file:
-            documents = yaml.dump(config_map, file)
         
-        tools.remplace_in_file("config.yaml", "'[", "[")
-        tools.remplace_in_file("config.yaml", "]'", "]")
-
-    return False
+        # Message(Successful): Is a directory
+        tools.message_successful(dir + " Is a directory")
+    else:
+        
+        # Message(Error): Not ss a directory
+        tools.message_error(dir + " Not is a directory")
+    
+    return done
 
 def write_property(config_obj, dir):
-    # pasos 
-    # 1.- identificar si el directorio es un NextPackage o NextProject
-    # 2.- buscar el archivo config.yaml
+    """Write a new Property
+
+    Args:
+        config_obj (Config_t): Configuration of project
+        dir (str): Directory
+
+    Returns:
+        flag: Flag successful
+    """
+    
+    # Identify if this is a directory
     if _this_is_a_dir(dir):
 
+        # Get Map of Config_object
         config_map = config_obj.to_map()
                 
+        # Write Config
         with open('config.yaml', 'w') as file:
             documents = config_obj.yaml.dump(config_map, file)
         
+        # Remplace chars '[' and ']'
         tools.remplace_in_file("config.yaml", "'[", "[")
         tools.remplace_in_file("config.yaml", "]'", "]")
+        
+        return True
 
     return False
