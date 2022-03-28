c4f48f4b7b0908a3509f003bc9ae65d96acf9a78
reitmas32 <rafa.zamora.rals@gmail.com>
2022-03-28 16:13:08 +0000

    Doc(version_next.py): Documentation completed and added CLI messages

--- src/version_next.py
+++ src/version_next.py
@@ -1,45 +1,56 @@
 ######################################################################
 ### author = Rafael Zamora 
 ### copyright = Copyright 2020-2022, Next Project 
-### date = 07/01/2022
+### date = 28/03/2022
 ### license = PSF
-### version = 3.0.0 
+### version = 3.2.0 
 ### maintainer = Rafael Zamora 
 ### email = rafa.zamora.ram@gmail.com 
 ### status = Production
 ######################################################################
 
-# Update 05/01/2022
-_VERSION = "3.0.0"
+# Update 28/03/2022
+_VERSION = "3.2.0"
 
 #System Packages
 import os
 
 #Local Packages
 import read_config
+import tools
 
 
 def version():
-    print("Next version: " + _VERSION)
+    # Message(Info): Next Version
+    tools.message_info("Next version: " + _VERSION)
 
 def version_all():
-    next_dir = ""
+    
+    # Next Packages Dierctory
+    next_packages_dir = ""
     try:
         #Search NEXT_PACKAGES_DIR
-        next_dir = os.environ['NEXT_PACKAGES_DIR']
-        print("NEXT_PACKAGES_DIR in:", next_dir)
+        next_packages_dir = os.environ['NEXT_PACKAGES_DIR']
+        
+        # Message(Info): NEXT_PACKAGES_DIR in:
+        tools.message_info("NEXT_PACKAGES_DIR in: " +  next_packages_dir)
 
     except:
-        #Not Find NEXT_PACKAGES_DIR
-        print("It was not found ENV NEXT_PACKAGES_DIR in func --version_next.version_all()--")
+        # Message(Error): Not Find NEXT_PACKAGES_DIR
+        tools.message_error("It was not found ENV NEXT_PACKAGES_DIR in func --version_next.version_all()--")
         exit()
 
-    list_next_packages = os.listdir(next_dir)
+    # Get Subdirectories 
+    list_next_packages = os.listdir(next_packages_dir)
 
     for next_pakage_dir in list_next_packages:
 
-        config_obj = read_config.read_config(next_dir+ "/" + next_pakage_dir)
+        # Read COnfig for DIrectory
+        config_obj = read_config.read_config(next_packages_dir+ "/" + next_pakage_dir)
 
+        # If exists Config_t
         if config_obj != False:
-            print(config_obj.name_project + " " + config_obj.version + "\n")
+            
+            # Message(Info): Get Version of Proects
+            tools.message_info(config_obj.name_project + " " + config_obj.version + "\n")
     
