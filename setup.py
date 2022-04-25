b1c2369ef8ecc9794755de3edc00d7a988e800f3
reitmas32 <rafa.zamora.rals@gmail.com>
2022-04-25 16:43:35 -0500

    feat(setup.py): Config setup.py file for PyPi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 ######################################################################
 ### author = Rafael Zamora 
 ### copyright = Copyright 2020-2022, Next Project 
-### date = 12/03/2022
+### date = 25/04/2022
 ### license = PSF
 ### version = 3.2.0
 ### maintainer = Rafael Zamora 
@@ -9,30 +9,76 @@
 ### status = Production
 ######################################################################
 
+#System Packages
+import os
 
+#Local Packages
+import src.version_next
+
+#Dependencies Packages
 from setuptools import setup
 
+def long_description_file():
+    """Genrete long descriptions
+
+    Returns:
+        str: long description
+    """
+    # Get current Directory
+    this_dir = os.getcwd()
+    
+    # Read file README.md
+    with open(this_dir + '/README.md', encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
 setup(
+    # Name to find the package in PyPi
     name = 'next-pm',
-    packages = ['src'],   
-    include_package_data=True,    # muy importante para que se incluyan archivos sin extension .py
-    version = '3.2.1',
+    
+    # Directory of code
+    packages = ['src'],
+    
+    # Import files without extension .py
+    include_package_data=True,
+    
+    # Version Current of Next
+    version = src.version_next.VERSION,
+    
+    # Short description
     description = 'Next es un administrador de proyectos de C/C++, es diseñado como una solucion a la administracion que requieren este tipo de proyectos.)',
+    
+    # long description
+    long_description=long_description_file(),
+    
+    # Name of Author
     author='Rafael Zamora',
+    
+    # email of Author
     author_email="rafa.zamora.ram@gmail.com",
+    
+    # License of proyect
     license="PSF",
+    
+    # Link of GitHub repository
     url="https://github.com/reitmas32/Next/tree/v3.2.0.pipy",
+    
+    # Classifiers of PyPi
     classifiers = ["Programming Language :: Python :: 3",\
         "License :: OSI Approved :: Python Software Foundation License",\
         "Development Status :: 5 - Production/Stable", "Intended Audience :: Developers", \
         "Operating System :: OS Independent", \
         "Topic :: Software Development :: Build Tools"],
+    
+    # Keywords 
     keywords=['C/C++', 'package', 'libraries', 'developer', 'manager',
               'dependency', 'tool', 'c', 'c++', 'cpp'],
+    
+    # Entry points
     entry_points={
         'console_scripts': [
             'next=src.main:main'
         ],
     }
     
-    )
+)
