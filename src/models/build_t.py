9fc37a8e40180344a5e0e8588acbf95d537d4a85
next-pm <next.pm.project@gmail.com>
2022-05-10 01:07:15 -0500

    feat(build_t.py): Config Build_t

--- src/models/build_t.py
+++ src/models/build_t.py
@@ -15,35 +15,60 @@ class Build_t:
             build_name (str): Name of Build
             config_obj (src.models.Config_t)
         """
+        
+        # Save the config_obj
         self.config_obj = config_obj
         
+        # Get Builds
         builds = config_obj.get('builds')
 
+        # Check that it is not None
         if build_name != None:
-            try:
-                self.config_build = builds[build_name]
             
-                if self.config_build['base'] == 'cmake':
-                    self.builder = src.builders.cmake_t.Cmake_t(self.config_build)
-            except KeyError as exc:
-                
-                # Message(Error): OSError generate
-                src.tools.message_error('The name of the requested build does not exist: ' + str(exc))
+            # Select the Builder
+            self._select_builder(build_name, builds)
         else:
-            self._none_name(config_obj)
-                
-    def _none_name(self, config_obj: src.config_t.config_t):
-        """Initialize a src.models.Build_t
+            
+            # Setup if no name explicit
+            self._none_name()
+            
+    def _select_builder(self, build_name: str, builds):
+        """ Select a Build
 
         Args:
             build_name (str): Name of Build
-            config_obj (src.models.Config_t)
+            builds (list: {}): List of Builds
         """
-        self.config_obj = config_obj
+        # Try get Build 
+        try:
+            
+            #Find Build
+            self.config_build = builds[build_name]
         
-        builds = config_obj.get('builds')
+            # Selctor of base of Build
+            if self.config_build['base'] == 'cmake':
+                self.builder = src.builders.cmake_t.Cmake_t(self.config_build)
+        except KeyError as exc:
+            
+            # Message(Error): OSError generate
+            src.tools.message_error('The name of the requested build does not exist: ' + str(exc))
+                
+    def _none_name(self):
+        """Initialize a src.models.Build_t without build_name explicit
+        """
+        
+        # Get a Builds
+        builds = self.config_obj.get('builds')
 
-        print("Set iene que usar el primer builder")
+        # Get a first builder
+        for builder in builds:
+            self._select_builder(builder, builds)
+            break
                 
     def build(self):
-        self.builder.build(self.config_obj)
+        
+        # check that it is not None
+        if self.builder != None:
+            
+            # Call to Build
+            self.builder.build(self.config_obj)
