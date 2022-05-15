f2be20d26e4db1aa4c6b06fbb1bd25dde432796e
next-pm <next.pm.project@gmail.com>
2022-05-15 13:03:46 -0500

    feat(dependencies.py): function that get the dependencies of the project

---
+++ src/funcs/dependencies.py
@@ -0,0 +1,23 @@
+import src.read_config
+
+
+def get_dependencies(route: str):
+    
+    list_dependencies = []
+    
+    try:
+        
+        # Read config of proyect
+        config_obj = src.read_config.read_config(route)
+        
+        dependencies = config_obj.get('dependencies')
+        
+        for name_include in dependencies:
+            
+            list_dependencies.append({name_include: dict(dependencies[name_include]) })
+
+    except OSError as exc:
+        # Message(Error): OSError generate
+        src.tools.message_error(str(exc))
+        
+    return list_dependencies
