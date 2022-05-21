9b7a51930c5ce5cfa27a9cf67236b265bb0e2c3e
reitmas32 <rafa.zamora.rals@gmail.com>
2022-05-21 00:13:13 -0500

    feat(src/models/tree/node_t.py): Create model to store the data of a tree

---
+++ src/models/tree/node_t.py
@@ -0,0 +1,16 @@
+from rich import Any
+
+
+class Node_t:
+    
+    data = None
+    childs = []
+    
+    def __init__(self, data):
+        self.data = data
+        
+    def new_child(self,child):
+        self.childs.append(child)
+        
+    def lenChilds(self):
+        return self.childs.count()
