a9eb48a264a2cc69f7ff05d2906733cb51d09a74
reitmas32 <rafa.zamora.rals@gmail.com>
2022-06-30 20:53:11 -0500

    feat(string.py): Create func for count the words in text

--- src/tool/string.py
+++ src/tool/string.py
@@ -3,3 +3,7 @@ import re
 def stripComments(code):
     code = str(code)
     return re.sub(r'(?m)^ *#.*\n?', '', code)
+
+def countWord(text: str, separator:str=" "):
+    text_list = text.split(sep=separator)
+    return len(text_list)
